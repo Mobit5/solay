97818485c93a1035e0aca8ff4ef27ba4957c2ad1
Mobit5 <91831754+Mobit5@users.noreply.github.com>
2025-07-04 23:10:25 +0300

    Add files via upload

---
+++ BACKENDSOLY/BACKENDSOLAY/config/middlewares.js
@@ -0,0 +1,19 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Authorization', 'Content-Type'],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
